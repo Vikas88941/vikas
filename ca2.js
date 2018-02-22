d5efd7e14391a1e1cd273bf2a9d79dea780114fd
Vikas88941 <35552306+Vikas88941@users.noreply.github.com>
2018-02-22 10:14:36 +0530

    Add files via upload

---
+++ ca2.js
@@ -0,0 +1,52 @@
+console.log("These three student not pass because passing marks criteria is 14 marks");
+var studentmarks=
+[
+{Student_name: "Vikas", subject_name: "cap115", TotalMarks:"30" ,average:"12"},
+,{Student_name: "Sahil Rai", subject_name: "cap734", TotalMarks:"30" ,average:"13"},
+,{Student_name: "Ashwini", subject_name: "cap733", TotalMarks:"30", average:"11"}
+
+];
+for(var key in studentmarks)
+{
+	console.log
+	(
+	"Student name :"+studentmarks[key].Student_name + " " +
+	"Student name :"+studentmarks[key].subject_name + " " +
+	"TotalMarks :"+studentmarks[key].TotalMarks + " " +
+	"average :"+studentmarks[key].average
+	);
+}
+console.log("----------------------------------------------------------------------------------------------------------");
+console.log("----------------------------------------------------------------------------------------------------------");
+
+console.log("Vishal pass this subject.He got 25 marks outoff 30 ");
+var StudentM=
+{
+	Student_Name: "Vishal",
+	subject_name: "cap121",
+	TotalMarks:"30",
+	average:"25"
+}
+console.log("student name :"+StudentM.Student_Name +" " +
+   "subject name :"+StudentM.subject_name + " " +
+   "totalmarks :"+StudentM.TotalMarks + " " +
+   "Average :"+StudentM.average)
+
+   
+ console.log("----------------------------------------------------------------------------------------------------------");  
+ console.log("----------------------------------------------------------------------------------------------------------");  
+console.log("Vishal pass this subject.He got 24 marks outoff 30 ");
+var StudentM1=
+{
+	Student_Name: "Rana",
+	subject_name: "cap120",
+	TotalMarks:"30",
+	average:"24"
+}
+console.log("student name :"+StudentM1.Student_Name +" " +
+   "subject name :"+StudentM1.subject_name + " " +
+   "totalmarks :"+StudentM1.TotalMarks + " " +
+   "Average :"+StudentM1.average)
+
+
+
